ec66e735f989552c2fcaeffbbfe4b5bc488ff8b7
Srinath Balakrishnan <srinathbalakrishnan08@gmail.com>
2026-07-10 19:21:27 +0530

    chore(frontend): downgrade to Expo SDK 52 and align Tailwind dependencies to fix Expo Go compatibility

--- expo-app/App.js
+++ expo-app/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Platform } from 'react-native';
 import HospitalDashboard from './components/HospitalDashboard';
 import DonorApp from './components/DonorApp';
-import "./global.css";
 
 export default function App() {
   if (Platform.OS === 'web') {
