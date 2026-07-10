79bec5cc0a93059b77225d57cd63ace5c183b065
Srinath Balakrishnan <srinathbalakrishnan08@gmail.com>
2026-07-10 20:11:06 +0530

    fix(frontend): completely remove NativeWind/reanimated/lucide, rewrite all components with StyleSheet, use platform-specific App files to isolate web/native bundles

--- expo-app/App.js
+++ expo-app/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import HospitalDashboard from './components/HospitalDashboard';
 import DonorApp from './components/DonorApp';
 
 export default function App() {
-  if (Platform.OS === 'web') {
-    return <HospitalDashboard />;
-  } else {
-    return <DonorApp />;
-  }
+  return <DonorApp />;
 }
